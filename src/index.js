2e873f28225ecd709be985b84e0d0e7f7cff1dab
Vlad <vvghost@ukr.net>
2020-09-28 00:15:54 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,67 @@ const hoursCounterRef = document.querySelector('[data-value="hours"]');
 const minutesCounterRef = document.querySelector('[data-value="mins"]');
 const secondsCounterRef = document.querySelector('[data-value="secs"]');
 const bodyRef = document.querySelector('body');
+const buttonStopRef = document.querySelector('.btn_stop');
+const buttonStartRef = document.querySelector('.btn_start');
 
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.dateInFuture = this.targetDate.getTime();
+    this.intervalId = null;
+    this.isActive = true;
+  }
+
+  updateClock(deltaTime) {
+    const days = this.pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(
+      Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)),
+    );
+    const secs = this.pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
+
+    daysCounterRef.textContent = days;
+    hoursCounterRef.textContent = hours;
+    minutesCounterRef.textContent = mins;
+    secondsCounterRef.textContent = secs;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  startCounter() {
+    if (this.isActive) {
+      this.isActive = false;
+      this.intervalId = setInterval(() => {
+        const currentTime = Date.now();
+        const differenceInTime = this.dateInFuture - currentTime;
+        this.updateClock(differenceInTime);
+        this.checkClock(currentTime);
+      }, 1000);
+    }
+  }
+
+  checkClock(currentTime) {
+    if (this.dateInFuture <= currentTime) {
+      const hpbElement = document.createElement('div');
+      hpbElement.textContent = 'Happy Birthday to me !';
+      hpbElement.classList.add('title');
+      bodyRef.appendChild(hpbElement);
+      clearInterval(this.intervalId);
+      daysCounterRef.textContent = 0;
+      hoursCounterRef.textContent = 0;
+      minutesCounterRef.textContent = 0;
+      secondsCounterRef.textContent = 0;
+    }
+  }
+
+  stopCounter() {
+    this.isActive = true;
+    clearInterval(this.intervalId);
   }
 }
 
@@ -18,45 +74,7 @@ const newTimer = new CountdownTimer({
   targetDate: new Date('May 20, 2021'),
 });
 
-const date = newTimer.targetDate;
-const dateInFuture = date.getTime();
+newTimer.startCounter();
 
-const intervalId = setInterval(() => {
-  const currentTime = Date.now();
-  const differenceInTime = dateInFuture - currentTime;
-
-  updateClock(differenceInTime);
-  checkClock(currentTime);
-}, 1000);
-
-const updateClock = deltaTime => {
-  const days = pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  const mins = pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
-
-  daysCounterRef.textContent = days;
-  hoursCounterRef.textContent = hours;
-  minutesCounterRef.textContent = mins;
-  secondsCounterRef.textContent = secs;
-};
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-
-function checkClock(currentTime) {
-  if (dateInFuture <= currentTime) {
-    const hpbElement = document.createElement('div');
-    hpbElement.textContent = 'Happy Birthday to me !';
-    hpbElement.classList.add('title');
-    bodyRef.appendChild(hpbElement);
-    clearInterval(intervalId);
-    daysCounterRef.textContent = 0;
-    hoursCounterRef.textContent = 0;
-    minutesCounterRef.textContent = 0;
-    secondsCounterRef.textContent = 0;
-  }
-}
+buttonStopRef.addEventListener('click', newTimer.stopCounter.bind(newTimer));
+buttonStartRef.addEventListener('click', newTimer.startCounter.bind(newTimer));
